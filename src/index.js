0426145754ab4d68c2266393e45f5efc36793725
bhaveymunjal <bhaveymunjal3@gmail.com>
2024-03-29 02:08:05 +0530

    fix cors error

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
-
+const corsOptions = {
+  origin:"https://taskify-bhavey.netlify.app/",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",};
+app.use(cors(corsOptions));
 app.use(cors()) 
 app.use(express.json());
 app.use(userRouter)
