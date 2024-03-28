0f51322f2d15be75640d8d3cfde34f65a8146130
bhaveymunjal <bhaveymunjal3@gmail.com>
2024-03-29 03:05:55 +0530

    Cors Fix

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,93 +1,11 @@
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 dotenv.config();
-// const validator = require("validator");
 
 const url = process.env.MONGODB_URL;
-mongoose.connect(url);
-
-// const User = mongoose.model("User", {
-//   name: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//   },
-//   age: {
-//     type: Number,
-//     validate(value) {
-//       if (value < 0) 
-//         throw new Error("Age must be positive");
-//     },
-//     default: 0,
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     validate(value) {
-//       if (!validator.isEmail(value)) 
-//         throw new Error("Email is invalid");
-//     },
-//     lowercase: true,
-//   },
-//   mobile: {
-//     type: String,
-//     validate(value) {
-//       if (!validator.isMobilePhone(value, "any", { strictMode: false }) || value.length !== 10) {
-//         throw new Error("Mobile Number is invalid");
-//       }
-//     },
-//     required: true,
-// },
-// password:{
-//     type: String,
-//     required: true,
-//     trim: true,
-//     validate(value){
-//         if (!validator.isLength(value, { min: 7 })) {
-//             throw new Error('Password must be at least 7 characters long.');
-//         }
-//         if (value.toLowerCase().includes('password')) {
-//             throw new Error('Password cannot contain the string "password".');
-//         }
-//     }
-//   }
-// });
-
-// const me = new User({
-//   name: "Vansh",
-//   //   age: -5,
-//   //   email: 'ankit@'
-//   email: "VB23@abc.co.in",
-//   mobile: "9466482404",
-//   password: 'frvdefef'
-// });
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// const Task = mongoose.model("Task", {
-//   description: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//   },
-//   isCompleted: {
-//     type: Boolean,
-//     default: false
-//   },
-// });
-
-// const work1 = new Task({
-//   description: "Description is again added",
-  // isCompleted: false,
-// });
-// work1
-//   .save()
-//   .then(() => console.log("Work 1 entry inserted"))
-//   .catch((err) => console.log(err));
-
-// REST API: Representational State Transfer - Application Programming Interface
+mongoose.connect(url).then(() => {
+  console.log("Connected to MongoDB");
+}
+).catch((err) => {
+  console.log(err);
+});
--- src/index.js
+++ src/index.js
@@ -8,28 +8,13 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
-// const corsOptions = {
-//   origin:"https://taskify-bhavey.netlify.app/",
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",};
-// app.use(cors(corsOptions));
+app.use(cors())
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://taskify-bhavey.netlify.app/"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
   next();
 });
 app.use(express.json());
@@ -39,144 +24,3 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log("Server is running on " + port);
 });
-
-// app.use((req, res, next)=>{
-//   if(req.method === 'GET'){
-//     res.send('GET requests are disabled')
-//   }
-//   else{
-//     next()
-//   }
-// })
-
-
-// Without middleware: new request -> run route handler
-// With middleware: new request -> do something -> run route handler 
-
-// app.use((req, res, next)=>{
-//   // res.status(503).send('Site is under maintenance. Please try again later')
-//   // console.log(req.method, req.path)
-//   // next()
-//   if(req.method === 'GET'){
-//     res.send('GET requests are disabled')
-//   }
-//   else{
-//     next()
-//   }
-// })
-
-
-// const Task = require('./models/task.js')
-// const User = require('./models/user.js')
-
-// const main = async ()=>{
-  // const task = await Task.findById('66008aa9445f66eca0574b8f')
-  // await task.populate('owner').then((task)=>{
-
-  // })
-  // console.log(task.owner)
-
-  // const user = await User.findById('660041d3a45fcc061d12a14f')
-  // await user.populate('tasks').then((user)=>{
-  //   // console.log(user)
-  // })
-  // console.log(user.tasks)
-// } 
-// main()
-
-
-
-// const myFunction = async ()=>{
-
-//   const bcrypt = require('bcryptjs');
-//   const password = 'Bhavey@6626'
-//   const hashedPassword = await bcrypt.hash(password, 8);
-//   console.log(password)
-//   console.log(hashedPassword)
-//   // const isMatch = await bcrypt.compare('Bhavey@6626', hashedPassword)
-//   const isMatch = await bcrypt.compare('bhavey@6626', hashedPassword)
-//   console.log(isMatch)
-// }
-// myFunction();
-
-// const jwt = require('jsonwebtoken');
-// const myFunction = async ()=>{
-//   const token = jwt.sign({_id: 'abc123'}, 'thisismynewcourse', {expiresIn: '7 days'})
-//   // console.log(token)
-//   const data = jwt.verify(token, 'thisismynewcourse')
-//   // console.log(data)
-// }
-// myFunction();
-
-// const pet = {
-//   name: 'Hal'
-// }
-
-// pet.toJSON = function(){
-//   return {}
-// }
-
-// console.log(JSON.stringify(pet))
-
-
-
-// app.post("/users", (req, res) => {
-//   //   res.send("Testing Web Server for users");
-//   const user = new User(req.body);
-//   user
-//     .save()
-//     .then(() => {
-//       res.status(201).send(user);
-//     })
-//     .catch((err) => {
-//       res.status(400).send(err);
-//     });
-// });
-
-// app.get("/users", (req, res) => {
-//   User.find({})
-//     .then((users) => res.send(users))
-//     .catch((err) => res.status(500).send(err));
-// });
-
-// app.get("/users/:id", (req, res) => {
-//   User.findOne({
-//     _id: new ObjectId(req.params.id),
-//   })
-//     .then((user) => {
-//       if (!user) res.status(404).send("User Not Found");
-//       res.send(user);
-//     })
-//     .catch((err) => res.status(500).send(err));
-// });
-
-
-// app.post("/tasks", (req, res) => {
-//   //   res.send("Testing Web Server for tasks");
-//   const task = new Task(req.body);
-//   task
-//     .save()
-//     .then(() => {
-//       res.status(201).send(task);
-//     })
-//     .catch((err) => {
-//       res.status(400).send(err);
-//     });
-// });
-
-// app.get("/tasks", (req, res) => {
-//   Task.find({})
-//     .then((tasks) => res.send(tasks))
-//     .catch((err) => res.status(500).send(err));
-// });
-
-// app.get("/tasks/:id", (req, res) => {
-//   Task.findOne({
-//     _id: new ObjectId(req.params.id),
-//   })
-//     .then((task) => {
-//       if (!task) res.status(404).send("User Not Found");
-//       res.send(task);
-//     })
-//     .catch((err) => res.status(500).send(err));
-// });
